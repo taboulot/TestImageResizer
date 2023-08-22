6db54f7630c3a6ceb92680c27905a614955ff737
Clément Taboulot <clement-taboulot@hotmail.fr>
2023-08-22 09:53:05 +0200

    test image resizing with 3.0.5

--- App.tsx
+++ App.tsx
@@ -8,112 +8,72 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import React, {useState} from 'react';
+import {Button, Image, SafeAreaView, StyleSheet, Text} from 'react-native';
+import ImageResizer from '@bam.tech/react-native-image-resizer';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [sourceUri, setSourceUri] = useState('');
+  const [resizedUri, setResizedUri] = useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const selectImageFromPicker = async () => {
+    launchImageLibrary({mediaType: 'photo'}, response => {
+      if (!response || !response.assets) return;
+      const asset = response.assets[0];
+      if (asset) {
+        setSourceUri(asset.uri ?? '');
+      }
+    });
+  };
+
+  const resizeImage = async () => {
+    if (!sourceUri) {
+      return;
+    }
+
+    const result = await ImageResizer.createResizedImage(
+      sourceUri,
+      960,
+      1280,
+      'JPEG',
+      80,
+    );
+
+    setResizedUri(result.uri);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Button title="Select Image" onPress={selectImageFromPicker} />
+      <Button title="Resize" onPress={resizeImage} />
+      <Text>Original image: </Text>
+      {sourceUri ? (
+        <Image
+          style={styles.image}
+          source={{uri: sourceUri}}
+          resizeMode="contain"
+        />
+      ) : null}
+      <Text>Resized image: </Text>
+      {resizedUri ? (
+        <Image
+          style={styles.image}
+          source={{uri: resizedUri}}
+          resizeMode="contain"
+        />
+      ) : null}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  image: {
+    height: 200,
+    width: 200,
   },
 });
 
